69ca536421104542a66f9c1b895c7e532a9661ac
Daniel <daniel.jorge96@outlook.com>
2024-04-02 19:28:39 -0300

    Información consistente entre los botones para agregar el estado de la materia en modo small y desktop

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 // Variables
 const d = document
 
-let $dropdownOpen = null
+let $dropdownOpen = null,
+  career = null
 
 // Funciones
 const openCloseMenu = function ($select, $caret, $menu) {
@@ -41,145 +42,82 @@ const getDataCareer = async function (careerName) {
   }
 }
 
-const loadInformationCareer = async function () {
-  try {
-    // Resolver esto para que llame directamente a getData
-    let res = await fetch('../data/plan-de-estudios.json'),
-      data = await res.json()
+const loadInformationTableDesing = async function () {
+  // const career = await getDataCareer('Licenciatura en Informática')
 
-    if (!res.ok)
-      throw {
-        status: res.status,
-        statusText: res.statusText,
+  const $fragment = d.createDocumentFragment(),
+    $article = d.getElementsByTagName('article')
+
+  const $titleCareer = d.getElementById('titleCareer'),
+    $careerDuration = d.getElementById('careerDuration')
+
+  // Esto se tiene que cargar una sola vez. Tanto para desktop como para mobile. Revisar.
+  $titleCareer.textContent = career.nombreCarrera
+  $careerDuration.textContent = `DURACIÓN: ${career.duracionCarrera} AÑOS`
+
+  if (career.hasOwnProperty('tituloIntermedio')) {
+    const $subCareer = d.getElementById('subCareer'),
+      $subCareerDuration = d.getElementById('subCareerDuration')
+
+    $subCareer.textContent = `${career.tituloIntermedio}`
+    $subCareerDuration.textContent = `DURACIÓN: ${career.duracionDelTituloIntermedio} AÑOS`
+  }
+
+  career.listaDeMateriasPorAnio.forEach((el, i) => {
+    const $templateTableInfo = d.getElementById('template-table'),
+      $cloneTemplateTableInfo = $templateTableInfo.cloneNode(true).content
+
+    $cloneTemplateTableInfo.querySelector('th').textContent +=
+      ` (${el.materias.length})`
+
+    $cloneTemplateTableInfo.querySelector('h2').textContent = el.anio
+
+    el.materias.forEach((subject, index) => {
+      const $templateTr = d.getElementById('tr-template'),
+        $cloneTemplateTr = $templateTr.cloneNode(true).content
+
+      $cloneTemplateTr
+        .querySelector('tr')
+        .classList.add(`${index % 2 ? 'bg-thirdColor' : 'bg-secondColor'}`)
+
+      $cloneTemplateTr.querySelector('tr').setAttribute('data-state', '')
+      $cloneTemplateTr.querySelector('tr').classList.add(`group${i}${index}`)
+
+      const $allTd = $cloneTemplateTr.querySelectorAll('td')
+
+      $allTd[0].textContent = subject.codigo
+      $allTd[1].textContent = subject.nombre
+      $allTd[2].textContent = subject.dictado
+      $allTd[4].appendChild(generateDropdown())
+
+      if (subject.correlativas.length != 0)
+        $allTd[3].textContent = subject.correlativas.join(' - ')
+
+      if (el.materias.length - 1 === index) {
+        $allTd[0].classList.add('rounded-bl-lg')
+        $allTd[4].classList.add('rounded-br-lg')
+
+        $allTd.forEach((td) => {
+          td.classList.remove('border-b-2')
+        })
       }
 
-    const career = data[0]
-
-    const $fragment = d.createDocumentFragment(),
-      $article = d.getElementsByTagName('article')
-
-    const $titleCareer = d.getElementById('titleCareer'),
-      $careerDuration = d.getElementById('careerDuration')
-
-    // Esto se tiene que cargar una sola vez. Tanto para desktop como para mobile. Revisar.
-    $titleCareer.textContent = career.nombreCarrera
-    $careerDuration.textContent = `DURACIÓN: ${career.duracionCarrera} AÑOS`
-
-    if (career.hasOwnProperty('tituloIntermedio')) {
-      const $subCareer = d.getElementById('subCareer'),
-        $subCareerDuration = d.getElementById('subCareerDuration')
-
-      $subCareer.textContent = `${career.tituloIntermedio}`
-      $subCareerDuration.textContent = `DURACIÓN: ${career.duracionDelTituloIntermedio} AÑOS`
-    }
-
-    career.listaDeMateriasPorAnio.forEach((el) => {
-      const $templateTableInfo = d.getElementById('template-table'),
-        $cloneTemplateTableInfo = $templateTableInfo.cloneNode(true).content
-
-      $cloneTemplateTableInfo.querySelector('th').textContent +=
-        ` (${el.materias.length})`
-
-      $cloneTemplateTableInfo.querySelector('h2').textContent = el.anio
-
-      el.materias.forEach((subject, index) => {
-        const $templateTr = d.getElementById('tr-template'),
-          $cloneTemplateTr = $templateTr.cloneNode(true).content
-
-        $cloneTemplateTr
-          .querySelector('tr')
-          .classList.add(`${index % 2 ? 'bg-thirdColor' : 'bg-secondColor'}`)
-
-        const $allTd = $cloneTemplateTr.querySelectorAll('td')
-
-        $allTd[0].textContent = subject.codigo
-        $allTd[1].textContent = subject.nombre
-        $allTd[2].textContent = subject.dictado
-
-        if (subject.correlativas.length != 0)
-          $allTd[3].textContent = subject.correlativas.join(' - ')
-
-        const $select = $allTd[4].querySelector('div > div'),
-          $caret = $select.querySelector('div'),
-          $menu = $allTd[4].querySelector('ul')
-
-        $select.id = `select-${subject.codigo}`
-
-        $select.addEventListener('click', () => {
-          if ($dropdownOpen !== null) {
-            if ($select === $dropdownOpen.$select) {
-              openCloseMenu($select, $caret, $menu)
-              $dropdownOpen = null
-              return
-            }
-
-            openCloseMenu(
-              $dropdownOpen.$select,
-              $dropdownOpen.$caret,
-              $dropdownOpen.$menu,
-            )
-          }
-
-          $dropdownOpen = {
-            $select,
-            $caret,
-            $menu,
-          }
-
-          openCloseMenu($select, $caret, $menu)
-        })
-
-        const $options = $allTd[4].querySelectorAll('ul > li'),
-          $selected = $allTd[4].querySelector('span')
-
-        $selected.setAttribute('id', `selected-${subject.codigo}`)
-
-        $options.forEach((option) => {
-          option.addEventListener('click', () => {
-            $selected.innerText = option.innerText
-
-            $options.forEach((option) => {
-              option.classList.remove('bg-[#23242a]')
-            })
-
-            option.classList.add('bg-[#23242a]')
-
-            openCloseMenu($select, $caret, $menu)
-            $dropdownOpen = null
-          })
-        })
-
-        if (el.materias.length - 1 === index) {
-          $allTd[0].classList.add('rounded-bl-lg')
-          $allTd[4].classList.add('rounded-br-lg')
-
-          $allTd.forEach((td) => {
-            td.classList.remove('border-b-2')
-          })
-        }
-
-        $cloneTemplateTableInfo
-          .querySelector('tbody')
-          .appendChild($cloneTemplateTr)
-      })
-
-      $fragment.appendChild($cloneTemplateTableInfo)
+      $cloneTemplateTableInfo
+        .querySelector('tbody')
+        .appendChild($cloneTemplateTr)
     })
 
-    $article[0].appendChild($fragment)
-  } catch (err) {
-    let message = err.statusText || 'Ocurrió un error'
-    console.log(`Error ${err.status}: ${message}`)
-  }
+    $fragment.appendChild($cloneTemplateTableInfo)
+  })
+
+  $article[0].appendChild($fragment)
 }
 
 const loadInformationSmallDesign = async function () {
-  const career = await getDataCareer('Licenciatura en Informática')
-
   const $fragment = d.createDocumentFragment(),
     $article = d.getElementsByTagName('article')[0]
 
-  career.listaDeMateriasPorAnio.forEach((el) => {
+  career.listaDeMateriasPorAnio.forEach((el, i) => {
     const $templateDesignSmall = d.getElementById(
         'design-small-template',
       ).content,
@@ -195,7 +133,7 @@ const loadInformationSmallDesign = async function () {
       .querySelector('div')
       .cloneNode()
 
-    el.materias.forEach((subject) => {
+    el.materias.forEach((subject, j) => {
       const $cloneSubjectInfomation = $templateDesignSmall
           .querySelector('div > div')
           .cloneNode(true),
@@ -203,6 +141,10 @@ const loadInformationSmallDesign = async function () {
           .querySelector('div')
           .querySelectorAll('p'),
         $dictado = $cloneSubjectInfomation.children[1].querySelector('p')
+
+      $cloneSubjectInfomation.classList.add(`group${i}${j}`)
+      $cloneSubjectInfomation.setAttribute('data-state', '')
+      $cloneSubjectInfomation.children[1].appendChild(generateDropdown())
 
       $nombreCodigoCorrelativas[0].textContent = subject.nombre
       $nombreCodigoCorrelativas[1].textContent = subject.codigo
@@ -238,8 +180,101 @@ const getStateOfSubjects = async function () {
   }
 }
 
-d.addEventListener('DOMContentLoaded', () => {
-  loadInformationCareer()
+const loadInformationLocal = async function () {
+  career = await getDataCareer('Licenciatura en Informática')
+}
+
+const updateBotton = function ($el, stateValue) {
+  const $selected = $el.querySelector('span'),
+    $allOptions = $el.querySelectorAll('li')
+
+  $selected.textContent = stateValue
+  $selected.dataset.option = stateValue
+
+  $allOptions.forEach((option) => {
+    option.classList.remove('bg-[#23242a]')
+  })
+
+  $allOptions.forEach((op) => {
+    if (op.dataset.option === stateValue) op.classList.add('bg-[#23242a]')
+  })
+}
+
+const generateDropdown = function () {
+  const $templateDropdown = d.getElementById('dropdown-template').content,
+    $cloneTemplateDropdown = $templateDropdown.cloneNode(true)
+
+  const $select = $cloneTemplateDropdown.querySelector('div > div'),
+    $caret = $select.querySelector('div'),
+    $menu = $cloneTemplateDropdown.querySelector('ul')
+
+  $select.addEventListener('click', () => {
+    if ($dropdownOpen !== null) {
+      if ($select === $dropdownOpen.$select) {
+        openCloseMenu($select, $caret, $menu)
+        $dropdownOpen = null
+        return
+      }
+      openCloseMenu(
+        $dropdownOpen.$select,
+        $dropdownOpen.$caret,
+        $dropdownOpen.$menu,
+      )
+    }
+    $dropdownOpen = {
+      $select,
+      $caret,
+      $menu,
+    }
+    openCloseMenu($select, $caret, $menu)
+  })
+
+  const $options = $cloneTemplateDropdown.querySelectorAll('ul > li'),
+    $selected = $cloneTemplateDropdown.querySelector('span')
+
+  $options.forEach((option) => {
+    option.addEventListener('click', (e) => {
+      $selected.innerText = option.innerText
+      $selected.dataset.option = option.dataset.option
+
+      $options.forEach((op) => {
+        op.classList.remove('bg-[#23242a]')
+      })
+
+      option.classList.add('bg-[#23242a]')
+      openCloseMenu($select, $caret, $menu)
+      $dropdownOpen = null
+
+      let currentElement = e.target
+
+      while (currentElement !== document.body) {
+        if (currentElement.hasAttribute('data-state')) {
+          currentElement.dataset.state = $selected.dataset.option
+
+          let classId =
+            currentElement.classList[currentElement.classList.length - 1]
+
+          updateBotton(
+            currentElement.tagName === 'DIV'
+              ? d.getElementsByClassName(classId)[0]
+              : d.getElementsByClassName(classId)[1],
+            currentElement.dataset.state,
+          )
+
+          break
+        }
+
+        currentElement = currentElement.parentNode
+      }
+    })
+  })
+
+  return $cloneTemplateDropdown
+}
+
+d.addEventListener('DOMContentLoaded', async () => {
+  await loadInformationLocal()
+  loadInformationTableDesing()
   loadInformationSmallDesign()
 })
 
